646150692eb75e05388653193d213235aa5f643b
luicalrob <luiscalderonr00@gmail.com>
2024-09-24 13:35:47 +0200

    create camera selection enum

--- Modules/Utils/CommonTypes.h
+++ Modules/Utils/CommonTypes.h
@@ -13,3 +13,9 @@ typedef shared_ptr<MapPoint> MapPoint_;
 typedef shared_ptr<KeyFrame> KeyFrame_;
 typedef shared_ptr<Eigen::Matrix3d> RotationMatrix_;
 typedef shared_ptr<Eigen::Vector3d> TranslationVector_;
+
+enum class cameraSelection {
+    C1,
+    C2,
+    Combined
+};
